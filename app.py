4ebb314ea0109f6a669ed3070e24ad799abd9b4e
marlemM <58227855+marlemM@users.noreply.github.com>
2024-10-22 04:55:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Carregar os dados do CSV
 data = pd.read_csv('dados_elemento.csv', sep=',')  # Altere 'seu_arquivo.csv' para o nome do seu arquivo
@@ -11,7 +12,7 @@ st.title('Visualização de Dados de Player')
 st.write("Dados Player:")
 st.dataframe(data)
 
-# Gráfico simples
+# Gráfico de barras
 st.bar_chart(data['placarA'])
 
 # Filtrar dados
@@ -20,3 +21,11 @@ dados_filtrados = data[data['playA'] == time_selecionado]
 
 st.write(f"Resultados para {time_selecionado}:")
 st.dataframe(dados_filtrados)
+
+# Gráfico de pizza
+placar_counts = dados_filtrados['placarA'].value_counts()
+fig, ax = plt.subplots()
+ax.pie(placar_counts, labels=placar_counts.index, autopct='%1.1f%%', startangle=90)
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+st.pyplot(fig)
