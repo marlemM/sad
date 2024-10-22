621c8f098eaf01ee26b14478e07d72858e0225ce
marlemM <58227855+marlemM@users.noreply.github.com>
2024-10-22 04:57:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
 
 # Carregar os dados do CSV
 data = pd.read_csv('dados_elemento.csv', sep=',')  # Altere 'seu_arquivo.csv' para o nome do seu arquivo
@@ -13,7 +11,7 @@ st.title('Visualização de Dados de Player')
 st.write("Dados Brutos:")
 st.dataframe(data)
 
-# Gráfico de barras
+# Gráfico simples
 st.bar_chart(data['placarA'])
 
 # Filtrar dados
@@ -22,17 +20,3 @@ dados_filtrados = data[data['playA'] == time_selecionado]
 
 st.write(f"Resultados para {time_selecionado}:")
 st.dataframe(dados_filtrados)
-
-# Gráfico de pizza usando Matplotlib
-placar_counts = dados_filtrados['placarA'].value_counts()
-fig, ax = plt.subplots()
-ax.pie(placar_counts, labels=placar_counts.index, autopct='%1.1f%%', startangle=90)
-ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-st.pyplot(fig)
-
-# Gráfico de pizza usando Plotly
-placar_counts_plotly = dados_filtrados['placarA'].value_counts().reset_index()
-placar_counts_plotly.columns = ['Placar', 'Frequência']
-
-fig_plotly = px.pie(placar_counts_plotly, values='Frequência', names='Placar', title='Distribuição dos Placar')
-st.plotly_chart(fig_plotly)
